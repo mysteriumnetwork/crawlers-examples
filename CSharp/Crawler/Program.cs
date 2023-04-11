66762cd58cad9f2ab65b699a4ef0f8cd49f63b81
Anton Litvinov <jnashicq@gmail.com>
2023-04-11 14:22:07 +0400

    Add a simple single-threaded example of crawler

---
+++ CSharp/Crawler/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace Crawler
+{
+    class Link
+    {
+        public string uri;
+        public int    depth;
+
+        public Link (string uri, int depth)
+        {
+            this.uri = uri;
+            this.depth = depth;
+        }
+
+        public override bool Equals(object ob)
+        {
+            if (ob is Link)
+            {
+                Link c = (Link)ob;
+                return depth == c.depth && uri == c.uri;
+            }
+            return false;            
+        }
+
+        public override int GetHashCode() {
+            return uri.GetHashCode() ^ depth.GetHashCode();
+        }
+    }
+
+    class Crawler
+    {
+        private Dictionary<string, Link> visited = new Dictionary<string, Link>();
+        private Queue<Link> jobs = new Queue<Link>();
+        private ISet<string> hosts = new HashSet<string>();
+        const int maxDepth = 1;
+        const int maxHosts = 5;
+
+        // maxHostsConstraint returns true if we have to skip the given link
+        private bool maxHostsConstraint(string e)
+        {
+            var uri = new Uri(e);           
+            if (!hosts.Contains(uri.Host))
+            {
+                if (hosts.Count() < maxHosts)
+                {
+                    hosts.Add(uri.Host);
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public ISet<string> collectLinks(string link)
+        {
+            var newLinks = new HashSet<string>();
+
+            var hw = new HtmlWeb();
+            var doc = hw.Load(link);
+            var nodes = doc.DocumentNode.SelectNodes("//a[@href]");
+            if (nodes != null)
+            {
+                foreach (HtmlNode node in nodes)
+                {
+                    var v = node.GetAttributeValue("href", "");
+                    try
+                    {
+                        var u = new Uri(v);
+                        newLinks.Add(v);
+                    }
+                    catch (System.UriFormatException) {}
+                }
+            }
+            return newLinks;
+        }
+
+        public void crawl(string u)
+        {
+            jobs.Enqueue(new Link(u, 0));
+
+            while (jobs.Count() > 0)
+            {
+                var j = jobs.Dequeue();
+
+                Console.WriteLine(string.Format("visit> {1} {0}", j.uri, j.depth));
+                var list = this.collectLinks(j.uri);
+                foreach (var e in list)
+                {
+                    if (!visited.ContainsKey(e))
+                    {
+                        if (this.maxHostsConstraint(e))
+                        {
+                            continue;
+                        }
+                        if (j.depth + 1 <= maxDepth)
+                        {
+                            var newJob = new Link(e, j.depth + 1);
+                            visited[e] = newJob;
+                            jobs.Enqueue(newJob);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Crawler();
+            c.crawl("http://google.com");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Crawler
+
+## CSharp
+To run this example:
+* install Visual Studio Community Edition 2019 or newer with C#
+* open the solution, press F7 to build
+* press Ctrl-F5 to run
+
